70a989e5766ff79f9ec926bee564e61394c8db04
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-04-21 07:30:03 +0000

    steam: home and collections tabs -> buttons & don't resize windows both were pretty broken and incompatible

--- dist/steam/steam.js
+++ dist/steam/steam.js
@@ -1,6 +1,3 @@
-import classes from "./classes.js";
-import ElementUtils from "../shared++/ElementUtils.js";
-
 function SetTokenAsVar(token) {
 	document.body.style.setProperty(
 		"--" + token,
@@ -8,52 +5,28 @@ function SetTokenAsVar(token) {
 	);
 }
 
-(async () => {
-	let el = (s) => document.querySelector(`.${s}`);
-
-	if (document.title == "Steam") {
-		// Align tabs with sidebar width
-		let sidebar = await ElementUtils.wait(
-			`.${classes.library.LeftListSizableContainer}`,
-		);
-		let tabs = await ElementUtils.wait(
-			`.${classes.gamelistbar.GameListHomeAndSearch}`,
-			sidebar,
-		);
-
-		ElementUtils.act(
-			sidebar,
-			() => {
-				tabs.style.marginLeft = `${sidebar.style.width} !important`;
-			},
-			{
-				attributes: true,
-				attributeFilter: ["style"],
+switch (document.title) {
+	case "Steam":
+		["AllCollectionsView_InfoIconCollections", "BottomBar_Manage"].forEach(
+			(e) => {
+				SetTokenAsVar(e);
 			},
 		);
+		break;
 
-		// Set a variable for when an application is downloading
-		SetTokenAsVar("BottomBar_Manage");
-	}
-
-	if (document.title.match(/^Screenshot(\s+)?Manager$/)) {
+	case "ScreenshotManager":
+	case "Screenshot Manager":
 		[
 			"Generic_Delete",
 			"ScreenshotUploader_CopyURL",
 			"ScreenshotUploader_Settings",
 			"ScreenshotUploader_ShowOnDisk",
-		].forEach((e) => SetTokenAsVar(e));
-	}
+		].forEach((e) => {
+			SetTokenAsVar(e);
+		});
+		break;
 
-	if (document.title == "Steam Settings") {
-		SteamClient.Window.ResizeTo(1010, 722, true);
-	}
-
-	if (document.title == "Game Servers") {
+	case "Game Servers":
 		SteamClient.Window.SetMinSize(500, 500);
-	}
-
-	if (document.title.startsWith("Game Info - ")) {
-		SteamClient.Window.ResizeTo(500, 600, true);
-	}
-})();
+		break;
+}
