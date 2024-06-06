92ca94d8a79c3bb9c7ac91df39b9865bfe454d01
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-06-06 07:17:10 +0000

    steam: fix js error & format

--- dist/steam/steam.js
+++ dist/steam/steam.js
@@ -1,33 +1,36 @@
-const LocalizeString = e => window.opener.LocalizationManager.LocalizeString(e)
+const { LocalizationManager } = window.opener;
 
 const tokens = (() => {
-		switch (document.title) {
-			case LocalizeString("#WindowName_SteamDesktop"):
-				return ["AllCollectionsView_InfoIconCollections", "BottomBar_Manage"];
-
-			case LocalizeString("#SignIn_Title"):
-				return ["NavigateBack"];
-
-			case LocalizeString("#ScreenshotUploader_Heading"):
-			case "ScreenshotManager": // Overlay
-				return [
-					"Generic_Delete",
-					"ScreenshotUploader_CopyURL",
-					"ScreenshotUploader_Settings",
-					"ScreenshotUploader_ShowOnDisk",
-				];
-		}
-	})();
-
-	for (const token of tokens) {
-		document.body.style.setProperty(
-			"--" + token,
-			"'" + LocalizeString("#" + token) + "'",
-		);
-	}
-
 	switch (document.title) {
-		case LocalizeString("#Menu_Servers"):
-			SteamClient.Window.SetMinSize(500, 500);
-			break;
+		case LocalizeString("#WindowName_SteamDesktop"):
+			return ["AllCollectionsView_InfoIconCollections"];
+
+		case LocalizeString("#SignIn_Title"):
+			return ["NavigateBack"];
+
+		case LocalizeString("#ScreenshotUploader_Heading"):
+		case "ScreenshotManager": // Overlay
+			return [
+				"Generic_Delete",
+				"ScreenshotUploader_CopyURL",
+				"ScreenshotUploader_Settings",
+				"ScreenshotUploader_ShowOnDisk",
+			];
+
+		default:
+			return [];
 	}
+})();
+
+for (const token of tokens) {
+	document.body.style.setProperty(
+		"--" + token,
+		"'" + LocalizationManager.LocalizeString("#" + token) + "'",
+	);
+}
+
+switch (document.title) {
+	case LocalizeString("#Menu_Servers"):
+		SteamClient.Window.SetMinSize(500, 500);
+		break;
+}
