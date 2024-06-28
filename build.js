e54b925f90dbd34c9e6bf35a777b369e505cb8c2
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-06-28 07:59:17 +0000

    build: use postcss

--- build.js
+++ build.js
@@ -1,5 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
+import postcss from "postcss";
+import postcssStyl from "postcss-styl";
 import stylus from "stylus";
 
 const SHARED_DIR = path.join("src", "shared");
@@ -11,6 +13,17 @@ const STEAM_CLASS_MAPS_FILE = "steam_class_maps.json";
 const readDir = (e) => fs.readdirSync(e).map((p) => path.join(e, p));
 const readFile = (e) => fs.readFileSync(e).toString();
 const mapText = (text, fn) => text.split("\n").map(fn).join("\n");
+const selectorReplacerPlugin = (opts) => (css) => {
+	css.walkRules((rule) => {
+		rule.selector = rule.selector.replace(opts.match, opts.replace);
+	});
+};
+selectorReplacerPlugin.postcss = true;
+const usePostcss = (opts, css) =>
+	postcss(selectorReplacerPlugin(opts)).process(css, {
+		from: undefined,
+		syntax: postcssStyl,
+	});
 
 const progs = fs.readdirSync("src").filter((e) => !e.startsWith("shared"));
 if (process.argv.length == 2 || !progs.some((e) => process.argv[2] == e)) {
@@ -60,24 +73,26 @@ let imports = [
 	.map((e) => `@import '${e}';`)
 	.join("\n");
 
-imports += (() => {
+const processedFiles = (() => {
 	switch (file) {
 		case "steam":
 			const importsDir = path.join("src", file, "imports");
 			const maps = JSON.parse(readFile(STEAM_CLASS_MAPS_FILE));
 			const keys = Object.keys(maps);
 
-			return fs
-				.readdirSync(importsDir)
-				.map((e) =>
-					readFile(path.join(importsDir, e)).replace(/#(\w+)/g, (_, s) => {
-						// TODO: doesn't work if directly passed to startsWith()
-						let klass = `${e.replace(".styl", "")}_${s}_`;
+			return fs.readdirSync(importsDir).map((e) =>
+				usePostcss(
+					{
+						match: /#(\w+)/g,
+						replace: (_, s) => {
+							const className = `${e.replace(".styl", "")}_${s}_`;
 
-						return "." + maps[keys.find((e) => e.startsWith(klass))];
-					}),
-				)
-				.join("\n");
+							return "." + maps[keys.find((e) => e.startsWith(className))];
+						},
+					},
+					readFile(path.join(importsDir, e)),
+				),
+			);
 
 		case "discord":
 			return readDir(rootDir)
@@ -88,22 +103,21 @@ imports += (() => {
 				.flatMap((e) => readDir(e))
 				.map((e) => readFile(e))
 				.map((e) =>
-					e
-						.split("\n")
-						.map((e) => e.replace(/\.(\w+)/g, '[class*="$1_"]'))
-						.map((e) =>
-							e.includes("@extend")
-								? e.replace(/\[class\*="(\w+)_"]/g, ".$1")
-								: e,
-						)
-						.join("\n"),
-				)
-				.join("\n");
+					usePostcss(
+						{
+							match: /\.(\w+)/g,
+							replace: '[class*="$1_"]',
+						},
+						e,
+					),
+				);
 
 		default:
-			return "";
+			return [];
 	}
 })();
+
+imports += (await Promise.all(processedFiles)).map((e) => e.css).join("\n");
 
 // Variables have to be manually edited due to a Stylus bug (or its age).
 const cssVars = readFile(path.join(SHARED_DIR, "variables.css")).replace(
