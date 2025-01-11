d316f2908e3e91e1c7e6d62680fcd7e3813393ac
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2025-01-11 15:08:09 +0000

    vscode: simplify js

--- dist/vscode/vscode.js
+++ dist/vscode/vscode.js
@@ -1,80 +1,31 @@
-const ElementUtils = {
-	/**
-	 * Create an element.
-	 *
-	 * @param tag HTML tag.
-	 * @param attrs Key/value attributes.
-	 * @param parent Element to prepend to.
-	 * @param prepend Prepend the element to its parent?
-	 */
-	make(tag, attrs, parent = null, prepend = false) {
-		const el = document.createElement(tag);
-		for (const k in attrs) {
-			el.setAttribute(k, attrs[k]);
-		}
-		if (parent) {
-			if (prepend) {
-				parent.prepend(el);
-			} else {
-				parent.appendChild(el);
-			}
-		}
-		return el;
-	},
-	/**
-	 * Do something on a DOM mutation event.
-	 *
-	 * @param el Element to look in.
-	 * @param callback The function to be called on DOM mutation.
-	 * @param opts MutationObserver options.
-	 */
-	act(el, callback, opts) {
-		const observer = new MutationObserver(callback);
-		observer.observe(el, opts);
-		return observer;
-	},
-	/**
-	 * Wait for an element.
-	 *
-	 * @param selector CSS selector.
-	 * @param parent Element to look in.
-	 */
-	wait(selector, parent = document) {
-		return new Promise((resolve) => {
-			const el = parent.querySelector(selector);
-			if (el) {
-				resolve(el);
-			}
-			const observer = new MutationObserver(() => {
-				const el = parent.querySelector(selector);
-				if (!el) {
-					return;
-				}
-				resolve(el);
-				observer.disconnect();
-			});
-			observer.observe(document.body, {
-				subtree: true,
-				childList: true,
-			});
-		});
-	},
-};
+const codiconStylesText = document
+	.getElementById("codiconStyles")
+	?.innerText.split("\n");
+
+/**
+ * Do something on a DOM mutation event.
+ *
+ * @param {Node} el Parent element.
+ * @param {MutationCallback} callback The function to be called on DOM mutation.
+ * @param {MutationObserverInit} opts MutationObserver options.
+ */
+function act(el, callback, opts) {
+	const observer = new MutationObserver(callback);
+	observer.observe(el, opts);
+
+	return observer;
+}
 
 function icon(s) {
-	console.log(
-		document
-			.getElementById("codiconStyles")
-			?.innerText.split("\n")
-			.filter((e) => e.match(s))
-			.map((e) => `&-${e.match(/^\.codicon-(.*?):/)[1]}`)
-			.join(",\n"),
-	);
+	return codiconStylesText
+		.filter((e) => e.match(s))
+		.map((e) => `&-${e.match(/^\.codicon-(.*?):/)[1]}`)
+		.join(",\n");
 }
 
 (async () => {
 	/**
-	 * Append the theme to shadow hosts.
+	 * Append the theme to shadow DOM hosts.
 	 **/
 	const container = await ElementUtils.wait(".monaco-workbench");
 
@@ -83,15 +34,14 @@ function icon(s) {
 		.find((e) => e.textContent.includes("color-button-face"))
 		.cloneNode(true);
 
-	ElementUtils.act(
+	act(
 		container,
 		() => {
 			if (!container.classList.contains("context-menu-visible")) {
 				return;
 			}
 
-			const hosts = [...container.querySelectorAll(".shadow-root-host")];
-
+			const hosts = container.querySelectorAll(".shadow-root-host");
 			for (const el of hosts) {
 				el.shadowRoot.appendChild(theme);
 			}
