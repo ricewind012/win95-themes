6df9de328d432197276800b0c41bb36daa5cf273
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2025-11-18 20:57:08 +0000

    feat(vscode): toolbars rework

--- src/vscode/vscode.js
+++ src/vscode/vscode.js
@@ -2,15 +2,34 @@
  * @import { ElementUtils } from "../shared++/ElementUtils.js"
  */
 
-const codiconStylesText = document
-	.getElementById("codiconStyles")
-	?.innerText.split("\n");
+// Icon generation
+const codicons = codiconStyles.innerText.split("\n");
+const iconMap = {
+	ea7c: ["overflow", "mono"],
+	ea76: ["close", "mono"],
+	ea81: ["clear", "normal"],
+	eabf: ["clear", "normal"],
+	eaf8: ["settings", "normal"],
+	eb37: ["reload", "normal"],
+	eb51: ["settings", "normal"],
+	eb84: ["list", "normal"],
+};
 
 window.icon = (s) => {
-	return codiconStylesText
+	return codicons
 		.filter((e) => e.match(s))
-		.map((e) => `&-${e.match(/^\.codicon-(.*?):/)[1]}`)
+		.slice(0, -1)
+		.map((e) => e.match(/^(\.codicon-.*?):/)[1])
 		.join(",\n");
+};
+
+window.generateCodiconMap = () => {
+	return Object.entries(iconMap)
+		.map(
+			([k, v]) =>
+				`${icon(k)} {\n\t--button-img: icon("${v[0]}", "${v[1]}");\n}`,
+		)
+		.join("\n\n");
 };
 
 // Append the theme to shadow DOM hosts.
