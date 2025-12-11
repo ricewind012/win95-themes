130abaee66305e3b66d28189b378fcafd6906768
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2025-12-11 08:47:26 +0000

    feat(steam): theme millennium settings

--- steam-theming-utils.config.js
+++ steam-theming-utils.config.js
@@ -1,6 +1,7 @@
 /** @type {import("steam-theming-utils").Config} */
 export default {
 	ignore: [
+		"steam/client/dialogs/settings/millennium",
 		"steam/client/popups/contextmenus/friendsui",
 	],
 };
